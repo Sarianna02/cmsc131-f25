f95417bba29f05bea60c08f0e509176195541334
FCC Dusel <citizen@Johns-MacBook-Pro.local>
2025-10-11 07:30:14 -0400

    FBK: Bank test coverage
    - test when addAccount fails
    - test when findAccountById fails
    - test data validation for `addAccount` and `findAccountById`

--- test/project/bank/BankTest.java
+++ test/project/bank/BankTest.java
@@ -6,17 +6,22 @@ import org.junit.jupiter.api.Test;
 
 public class BankTest {
 
+    // TODO test when addAccount fails
+    // TODO test when findAccontById fails
+    // TODO test data validation in addAccount and findAccountByID
+
     @Test
     public void testAddAccountAndCount(){
         Bank bank = new Bank();
         Account a1 = new Account("405", "Danelle", 350.00, "checking");
         bank.addAccount(a1);
         Account result = bank.findAccountById("405");
-        assertNotNull(result);
+        assertNotNull(result); // compile error
         assertEquals("Danelle", result.getOwnerName());
     
     }
 
+    // looks like work-in-progress
     @Test
     public void 
 
